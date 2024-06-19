026074e92cbe577deb30e23e74da0d0f6298dbd4
Sperling-0 <tanujadoriya@gmail.com>
2024-06-19 14:11:16 +0200

    Third commit base_branch
    - Desc 3

---
+++ Kernel.php
@@ -0,0 +1,51 @@
+<?php
+
+declare( strict_types = 1 );
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+class Kernel extends BaseKernel {
+
+	use MicroKernelTrait;
+
+	protected function configureContainer( ContainerConfigurator $container ): void {
+		$container->import( '../config/{packages}/*.yaml' );
+		$container->import( '../config/{packages}/' . $this->environment . '/*.yaml' );
+
+		if ( is_file( \dirname( __DIR__ ) . '/config/services.yaml' ) ) {
+			$container->import( '../config/services.yaml' );
+			$container->import( '../config/{services}_' . $this->environment . '.yaml' );
+		}
+	}
+
+	/**
+	 * @param string $routeName
+	 * @param array<string, scalar> $parameters
+	 */
+	public function generateRelativeUrl( string $routeName, array $parameters = [] ): string;
+
+	protected function configureRoutes( RoutingConfigurator $routes ): void {
+		$routes->import( '../config/routes.yaml' );
+		$routes->import( '../config/api-routes.yaml' );
+	}
+
+	public function getCacheDir(): string {
+		// Use /tmp in Docker images, because it's world-writable
+		if ( $this->environment === 'dev' || $this->environment === 'test' ) {
+			return "/tmp/symfony/cache/{$this->environment}";
+		}
+		return parent::getCacheDir();
+	}
+
+	public function getLogDir(): string {
+		// Use /tmp in Docker images, because it's world-writable
+		if ( $this->environment === 'dev' || $this->environment === 'test' ) {
+			return "/tmp/symfony/log/{$this->environment}";
+		}
+		return parent::getLogDir();
+	}
+
+}
